63251b16d6b69198d2ffa9360d646a35851c7729
Haris <hh063063063@gmail.com>
2023-09-07 02:51:50 -0700

    Work Completed

---
+++ app.js
@@ -0,0 +1,184 @@
+//chapter no 1
+
+// Q1
+
+var name1 ="Muhammad \n Fareed \n ff063063063@gmail.com \n 03041025570 \n Passoward" 
+
+alert(name1)
+
+
+// chapter no 2
+
+// Q1
+
+
+var camelCase="firstName"
+
+alert(camelCase)
+
+// Q 2
+
+var fullName;
+
+fullName ="Muhammad Fareed"
+
+alert(fullName)
+
+//Q 4
+
+var friend="Faisal"
+
+alert(friend)
+
+
+// Chapter no 3 
+
+// Q1
+
+var add = 20;
+
+var nam = "20";
+
+alert(add * nam);
+
+// Q2
+
+var Total = 45;
+
+var fee = 15;
+
+var result = Total + fee
+
+alert(result)
+
+// Q3
+
+var ali = 120;
+
+var faraz = 150;
+
+var total_point = ali +faraz 
+
+alert(total_point)
+
+// chapter no 4
+
+// Q1
+
+var chapter="chapter no 4"
+
+var  productcost="Q1. productcost wrong or productcostcorrect"
+
+document.write(chapter+ "<br>"+productcost)
+
+// Q2
+
+
+var legal="Q2. Nameofband legal"
+
+var sp="&"
+
+var illegal="Nameofband illegal"
+
+document.write("<br>"+ legal+ "   "+ sp + "   " +illegal)
+
+
+// Q3
+
+var outfiter$ = "Q3. Shirts"
+
+document.write("<br>" + outfiter$)
+
+// Q4 
+
+var camelCase = "Q4. Hello Friend"
+
+document.write("<br>" + camelCase)
+
+
+// Q5 
+
+var legal1 = "Q5. 01. camelCase 02. number1 03. camel_case 04. Dollar$"
+
+var illegal1 = "Q5/1. name of band 02. name@ 03. number"
+
+document.write("<br>" + legal1 + "<br>" + illegal1)
+
+// chapter no 5
+
+// Q1
+
+
+
+var chapter = "Chapter no 05";
+
+var nu_m = 30;
+
+var num_1 = 8;
+
+var res ="Q1." + " " + nu_m % num_1
+
+document.write("<br>" + chapter + "<br>" + res)
+
+// Q2
+
+var multiPlied = 137;
+
+var multiPlied1 = 298;
+
+var res1 = "Q2." + " " + multiPlied * multiPlied1
+
+document.write("<br>" + res1)
+
+
+// Q3
+
+
+var question = "Q3."
+
+var number_3 = 352;
+
+var number_2 = 325;
+
+var res_2 = number_3 - number_2
+
+document.write("<br>" + "  " + question + res_2)
+
+
+
+
+
+// Q4
+
+var question_1 = "Q4."
+
+var divided = 89;
+
+var divided_1 = 65;
+
+var res_3 = divided / divided_1
+
+document.write("<br>" + question_1 + res_3)
+
+// Q5
+
+var question_2 = "Q5."
+
+var multiplication = 23;
+
+var multiplication_1 = 89;
+
+var res_4 = multiplication * multiplication_1
+
+var end = "End is Here"
+
+alert( question_2 + res_4 )
+
+document.write("<br>" + "Assigment End Is Here")
+
+
+
+
+
+
+
